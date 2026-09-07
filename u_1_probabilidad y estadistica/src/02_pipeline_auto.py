b8fdee57f320a60429945460604c8a03fa37791d
melanie <mmm82418@lasallebajio.edu.mx>
2026-09-07 16:33:56 -0600

    Agrego avances en transformaciones.ipynb

---
+++ u_1_probabilidad y estadistica/src/02_pipeline_auto.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import requests
+
+# ============================================================
+# 1. EXTRACCIÓN — Fetch the data
+# ============================================================
+
+import pandas as pd
+import requests
+
+# Fetch the data (nuevo)
+df = pd.read_csv("https://ourworldindata.org/grapher/children-born-per-woman.csv?v=1&csvType=full&useColumnShortNames=true", storage_options = {'User-Agent': 'Our World In Data data fetch/1.0'})
+
+# Fetch the metadata(nuevo)
+metadata = requests.get("https://ourworldindata.org/grapher/children-born-per-woman.metadata.json?v=1&csvType=full&useColumnShortNames=true").json()
+
+# ============================================================
+# 3. EXPLORACIÓN INICIAL
+# ============================================================
+
+print("Dimensiones del DataFrame:")
+print(df.shape)
+
+print("\nColumnas:")
+print(df.columns.tolist())
+
+print("\nTipos de datos:")
+print(df.dtypes)
+
+print("\nValores nulos:")
+print(df.isnull().sum())
+
+# ============================================================
+# 4. LIMPIEZA
+# ============================================================
+
+# Eliminar registros que no tengan código de país
+df = df.dropna(subset=["code"])
+
+# Eliminar registros que no tengan esperanza de vida
+df = df.dropna(subset=["life_expectancy_0"])
+
+# ============================================================
+# 5. TRANSFORMACIÓN
+# ============================================================
+
+# Convertir el año a entero
+df["Year"] = df["year"].astype(int)
+
+# Crear una nueva columna con la esperanza de vida
+# redondeada a dos decimales
+df["life_expectancy_round"] = df["life_expectancy_0"].round(2)
+
+# ============================================================
+# 6. FILTRADO
+# ============================================================
+
+# Trabajaremos únicamente con datos a partir del año 2000
+df = df[df["year"] >= 2000]
+
+# ============================================================
+# 7. ANÁLISIS
+# ============================================================
+
+# Promedio de esperanza de vida por país
+promedio_pais = (
+    df.groupby("entity")["life_expectancy_round"]
+      .mean()
+      .round(2)
+      .sort_values(ascending=False)
+)
+
+print("\nPromedio de esperanza de vida por país:")
+print(promedio_pais.head(10))
+
+# ============================================================
+# 8. RESULTADO
+# ============================================================
+archivo_salida = "data/processed/promedio_esperanza_vida_por_pais.csv"
+
+promedio_pais.to_csv(
+    archivo_salida,
+    header=["Average life expectancy"]
+)
+
+print("\nPipeline ejecutado correctamente.")
+print(f"Archivo generado: {archivo_salida}")
